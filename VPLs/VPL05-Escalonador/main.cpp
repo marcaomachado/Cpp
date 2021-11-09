19e05de7397f629d8791b8982f336536e8c6176f
Marco Túlio Machado <marcotuliomachadofranca@gmail.com>
2021-11-09 18:54:50 -0300

    Adicionando Makefile

--- VPLs/VPL05-Escalonador/main.cpp
+++ VPLs/VPL05-Escalonador/main.cpp
@@ -1,5 +1,5 @@
 // NÃO ALTERE ESSA LINHA
-#include "avaliacao_basica_escalonador.hpp"
+//#include "avaliacao_basica_escalonador.hpp"
 
 #include <iostream>
 #include "Processo.hpp"
@@ -18,39 +18,39 @@ int main()
     {
         switch (entrada)
         {
-        case 'a':
-        {
-            int id;
-            string nome;
-            int prioridade;
+            case 'a':
+            {
+                int id;
+                string nome;
+                int prioridade;
 
-            cin >> id >> nome >> prioridade;
+                cin >> id >> nome >> prioridade;
 
-            Processo* aux = new Processo(id, nome, prioridade);
+                Processo* aux = new Processo(id, nome, prioridade);
 
-            lista.adicionar_processo(aux);
-            break;
-        }
+                lista.adicionar_processo(aux);
+                break;
+            }
 
-        case 'm':
-            lista.remover_processo_maior_prioridade();
-            break;
-        case 'n':
-            lista.remover_processo_menor_prioridade();
-            break;
-        case 'r':
-            int id;
-            cin >> id;
-            lista.remover_processo_por_id(id);
-            break;
-        case 'p':
-            lista.imprimir_lista();
-            break;
-        case 'b':
-            avaliacao_basica();
-            break;
-        default:
-            break;
+            case 'm':
+                lista.remover_processo_maior_prioridade();
+                break;
+            case 'n':
+                lista.remover_processo_menor_prioridade();
+                break;
+            case 'r':
+                int id;
+                cin >> id;
+                lista.remover_processo_por_id(id);
+                break;
+            case 'p':
+                lista.imprimir_lista();
+                break;
+            case 'b':
+                //avaliacao_basica();
+                break;
+            default:
+                break;
         }
     }
 
